090e2cf5c9bb507b5828cfe793315497584a61e5
Em Ji Madhu <emjimadhu@gmail.com>
2020-02-23 12:14:59 +0530

    Add `controller` test case

--- src/libs/controller/index.spec.ts
+++ src/libs/controller/index.spec.ts
@@ -1,5 +1,4 @@
-import { Tree } from '@angular-devkit/schematics';
-import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 
 import { IControllerOptions } from './schema';
@@ -9,12 +8,140 @@ const collectionPath = path.join(__dirname, '../../collection.json');
 describe('Alosaur Controller', () => {
   const runner = new SchematicTestRunner('.', collectionPath);
 
-  it('works', () => {
+  it('should manage name only', () => {
     const options: IControllerOptions = {
       name: 'foo'
     };
-    const tree = runner.runSchematic('controller', options, Tree.empty());
+    const tree: UnitTestTree = runner.runSchematic('controller', options);
+    const files: string[] = tree.files;
+    expect(
+      files.find(filename => filename === '/foo/foo.controller.ts'),
+    ).not.toBeUndefined();
+    expect(tree.readContent('/foo/foo.controller.ts')).toEqual(
+      'import { Controller, Content, Get } from \'alosaur/mod.ts\';\n' +
+            '\n' +
+            '@Controller(\'/foo\')\n' +
+            'export class FooController {\n' +
+            '  @Get(\'/text\')\n' +
+            '  public text() {\n' +
+            '    return Content(\'Hello world\');\n' +
+            '  }\n' +
+            '\n' +
+            '  @Get(\'/json\')\n' +
+            '  public json() {\n' +
+            '    return Content({ text: \'test\' });\n' +
+            '  }\n' +
+            '}\n'
+    );
+  });
 
-    expect(tree.files).toEqual([]);
+  it('should manage name as a path', () => {
+    const options: IControllerOptions = {
+      name: 'bar/foo'
+    };
+    const tree: UnitTestTree = runner.runSchematic('controller', options);
+    const files: string[] = tree.files;
+    expect(
+      files.find(filename => filename === '/bar/foo/foo.controller.ts'),
+    ).not.toBeUndefined();
+    expect(tree.readContent('/bar/foo/foo.controller.ts')).toEqual(
+      'import { Controller, Content, Get } from \'alosaur/mod.ts\';\n' +
+            '\n' +
+            '@Controller(\'/foo\')\n' +
+            'export class FooController {\n' +
+            '  @Get(\'/text\')\n' +
+            '  public text() {\n' +
+            '    return Content(\'Hello world\');\n' +
+            '  }\n' +
+            '\n' +
+            '  @Get(\'/json\')\n' +
+            '  public json() {\n' +
+            '    return Content({ text: \'test\' });\n' +
+            '  }\n' +
+            '}\n'
+    );
+  });
+
+  it('should manage name and path', () => {
+    const options: IControllerOptions = {
+      name: 'foo',
+      path: 'bar'
+    };
+    const tree: UnitTestTree = runner.runSchematic('controller', options);
+    const files: string[] = tree.files;
+    expect(
+      files.find(filename => filename === '/bar/foo/foo.controller.ts'),
+    ).not.toBeUndefined();
+    expect(tree.readContent('/bar/foo/foo.controller.ts')).toEqual(
+      'import { Controller, Content, Get } from \'alosaur/mod.ts\';\n' +
+            '\n' +
+            '@Controller(\'/foo\')\n' +
+            'export class FooController {\n' +
+            '  @Get(\'/text\')\n' +
+            '  public text() {\n' +
+            '    return Content(\'Hello world\');\n' +
+            '  }\n' +
+            '\n' +
+            '  @Get(\'/json\')\n' +
+            '  public json() {\n' +
+            '    return Content({ text: \'test\' });\n' +
+            '  }\n' +
+            '}\n'
+    );
+  });
+
+  it('should manage name to dasherize\'', () => {
+    const options: IControllerOptions = {
+      name: 'fooBar'
+    };
+    const tree: UnitTestTree = runner.runSchematic('controller', options);
+    const files: string[] = tree.files;
+    expect(
+      files.find(filename => filename === '/foo-bar/foo-bar.controller.ts'),
+    ).not.toBeUndefined();
+    expect(tree.readContent('/foo-bar/foo-bar.controller.ts')).toEqual(
+      'import { Controller, Content, Get } from \'alosaur/mod.ts\';\n' +
+            '\n' +
+            '@Controller(\'/foo-bar\')\n' +
+            'export class FooBarController {\n' +
+            '  @Get(\'/text\')\n' +
+            '  public text() {\n' +
+            '    return Content(\'Hello world\');\n' +
+            '  }\n' +
+            '\n' +
+            '  @Get(\'/json\')\n' +
+            '  public json() {\n' +
+            '    return Content({ text: \'test\' });\n' +
+            '  }\n' +
+            '}\n'
+    );
+  });
+
+  it('should not create a directory if "flat" is true', () => {
+    const options: IControllerOptions = {
+      name: 'foo',
+      flat: true
+    };
+    const tree: UnitTestTree = runner.runSchematic('controller', options);
+    const files: string[] = tree.files;
+    expect(
+      files.find(filename => filename === '/foo.controller.ts'),
+    ).not.toBeUndefined();
+    expect(tree.readContent('/foo.controller.ts')).toEqual(
+      'import { Controller, Content, Get } from \'alosaur/mod.ts\';\n' +
+            '\n' +
+            '@Controller(\'/foo\')\n' +
+            'export class FooController {\n' +
+            '  @Get(\'/text\')\n' +
+            '  public text() {\n' +
+            '    return Content(\'Hello world\');\n' +
+            '  }\n' +
+            '\n' +
+            '  @Get(\'/json\')\n' +
+            '  public json() {\n' +
+            '    return Content({ text: \'test\' });\n' +
+            '  }\n' +
+            '}\n'
+    );
   });
 });

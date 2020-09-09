304979417ec3e178815b86f2db33f07279d8172e
Em Ji Madhu <emjimadhu@gmail.com>
2020-09-09 08:59:22 +0530

    refactor: :recycle: static boilerplate

--- src/libs/app/files/static/app.ts
+++ src/libs/app/files/static/app.ts
@@ -1,15 +1,51 @@
-import { App } from 'alosaur/mod.ts';
+import {
+  Cookie,
+  setCookie,
+  getCookies,
+} from "http/cookie.ts";
+import { Response } from "http/server.ts";
+import {
+  App,
+  Area,
+  Controller,
+  Get,
+  Req,
+  Res,
+  ServerRequest,
+} from "alosaur/mod.ts";
+import { Redirect } from "alosaur/src/renderer/redirect.ts";
 
-import { HomeArea } from './areas/home/home.area.ts';
+@Controller("/home")
+export class HomeController {
+  @Get("/")
+  default(@Res() response: Response) {
+    const cookie: Cookie = { name: "name", value: "Cat" };
+    setCookie(response, cookie);
+
+    return Redirect("/home/text");
+  }
+
+  @Get("/text")
+  text(@Req() request: ServerRequest) {
+    const cookies = getCookies(request);
+
+    return `Hey! ${cookies["name"]}`;
+  }
+}
+
+@Area({
+  controllers: [HomeController],
+})
+export class HomeArea {}
 
 const app = new App({
-  areas: [HomeArea]
+  areas: [HomeArea],
 });
 
 app.useStatic({
-    root: `${Deno.cwd()}/examples/static/www`,
-    index: "index.html",
-    baseRoute: '/www/' // or undefined for default route /
-});
+  root: `${Deno.cwd()}/examples/static/www`,
+  index: "index.html",
+  baseRoute: "/www/",
+} // or undefined for default route /
+);
 app.listen();
-
--- src/libs/app/files/static/areas/home/home.area.ts
+++
@@ -1,9 +0,0 @@
-import { Area } from 'alosaur/mod.ts';
-
-import { HomeController } from './home.controller.ts';
-
-@Area({
-  controllers: [HomeController]
-})
-export class HomeArea {
-}
